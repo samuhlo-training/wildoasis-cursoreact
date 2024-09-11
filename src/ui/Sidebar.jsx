d038b36ddfce25608cfdb50c64effb6529351413
samU13 <samu13lop@gmail.com>
2024-09-11 17:49:28 +0200

    upload admin fix

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
-// import Uploader from "../data/Uploader";
+import Uploader from "../data/Uploader";
+import { getCurrentUser } from "../services/apiAuth";
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -15,11 +17,21 @@ const StyledSidebar = styled.aside`
 `;
 
 function Sidebar() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    async function fetchUser() {
+      const currentUser = await getCurrentUser();
+      setUser(currentUser);
+    }
+
+    fetchUser();
+  }, []);
   return (
     <StyledSidebar>
       <Logo />
       <MainNav />
-        {/* <Uploader/>  */}
+      {user?.email === "samu@example.com" && <Uploader />}
     </StyledSidebar>
   );
 }
